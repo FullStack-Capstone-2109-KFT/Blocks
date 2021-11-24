c3e1eb0987004743d13a7dbd5d870844aa4e8ef0
Davide Lepri <davidelepri3@gmail.com>
2021-11-24 16:25:19 -0500

    contract

--- contracts/Blocks.sol
+++ contracts/Blocks.sol
@@ -1,17 +1,50 @@
-pragma solidity ^0.5.0;
+pragma solidity ^0.8.10;
 
 contract Blocks {
     string public name = 'Blocks';
-    //Number of files
-    //Mapping fileId => Struct
-    
-    //Struct
-    
-    //Event
+    uint public fileCount = 0;
+    mapping(uint => File) public files; // mapping number of files through our Struct;
+
+    struct File { // file model that will be uploaded from users
+        uint id;
+        string fileHash;
+        uint fileSize;
+        string fileType;
+        string fileName;
+        string fileDescription;
+        uint uploadTime;
+        address uploader;
+    }
+
+    event FileUploaded (
+        uint id,
+        string fileHash,
+        uint fileSize,
+        string fileType,
+        string fileName,
+        string fileDescription,
+        uint uploadTime,
+        address uploader
+    );
+
+    // constructor() public { // when the contract is first deployed initialize state variable
+
+    // }
+
+    function uploadFile(string memory _fileHash, uint _fileSize, string memory _fileType, string memory _fileName, string memory _fileDescription) public {
+
+    require(bytes(_fileHash).length > 0); // validation for uploaded file from user
+    require(bytes(_fileType).length > 0);
+    require(bytes(_fileDescription).length > 0);
+    require(bytes(_fileName).length > 0);
+    require(msg.sender != address(0));
+
+    require(_fileSize > 0);
+    fileCount++;
+    // string calldata mess = msg.sender;
+    files[fileCount] = File(fileCount, _fileHash, _fileSize, _fileType, _fileName, _fileDescription, block.timestamp, msg.sender);
+
+    emit FileUploaded(fileCount, _fileHash, _fileSize, _fileType, _fileName, _fileDescription, block.timestamp, msg.sender);
+    }
 }
 
-constuctor() public {
-
-}
-
-//Upload File function
