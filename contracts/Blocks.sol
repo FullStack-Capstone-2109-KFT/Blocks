5b7fde0cde2f6913fe83ee39cadd51c4d7eb74f3
totsuka1 <takumi.otsuka@gmail.com>
2021-11-30 18:31:54 -0500

    contract testing w/ user struct

--- contracts/Blocks.sol
+++ contracts/Blocks.sol
@@ -1,62 +1,88 @@
 pragma solidity ^0.8.10;
 
 contract Blocks {
-    string public name = 'Blocks';
-    // uint public fileCount = 0;
-    mapping(uint => User) public users;
-    // mapping(uint => File) public files;
-    
-    struct User {
-        uint id;
-        uint fileCount;
-        File[] files;
-    }
-    
-    struct File {
-        uint id;
-        string fileHash;
-        uint fileSize;
-        string fileType;
-        string fileName;
-        string fileDescription;
-        uint uploadTime;
-        address uploader;
-    }
+  string public name = "Blocks";
+  // uint public fileCount = 0;
+  mapping(uint256 => User) public users;
+  // mapping(uint => File) public files;
 
-    event FileUploaded (
-        uint id,
-        uint userId
-        string fileHash,
-        uint fileSize,
-        string fileType,
-        string fileName,
-        string fileDescription,
-        uint uploadTime,
-        address uploader
-    );
+  struct User {
+    uint256 id;
+    uint256 fileCount;
+    // File[] files;
+  }
 
-    // constructor() public { // when the contract is first deployed initialize state variable
+  struct File {
+    uint256 id;
+    string fileHash;
+    uint256 fileSize;
+    string fileType;
+    string fileName;
+    string fileDescription;
+    uint256 uploadTime;
+    address uploader;
+  }
 
-    // }
+  event FileUploaded(
+    uint256 id,
+    uint256 userId,
+    string fileHash,
+    uint256 fileSize,
+    string fileType,
+    string fileName,
+    string fileDescription,
+    uint256 uploadTime,
+    address uploader
+  );
 
-    function uploadFile(uint _userId, string memory _fileHash, uint _fileSize, string memory _fileType, string memory _fileName, string memory _fileDescription) public {
-    
-    require(bytes(_userId) > 0);
+  // constructor() public { // when the contract is first deployed initialize state variable
+
+  // }
+
+  User user1 = User(1, 2);
+
+  function uploadFile(
+    uint256 _userId,
+    string memory _fileHash,
+    uint256 _fileSize,
+    string memory _fileType,
+    string memory _fileName,
+    string memory _fileDescription
+  ) public {
+    require(_userId > 0);
     require(bytes(_fileHash).length > 0);
     require(bytes(_fileType).length > 0);
     require(bytes(_fileDescription).length > 0);
     require(bytes(_fileName).length > 0);
     require(msg.sender != address(0));
     require(_fileSize > 0);
-    
-    uint user = users[_userId]
-    user.fileCount ++
-     
+
+    user1 = users[_userId];
+    // user.fileCount ++;
+
     // string calldata mess = msg.sender;
-    
-    user.File[user.fileCount] = File(fileCount, _fileHash, _fileSize, _fileType, _fileName, _fileDescription, block.timestamp, msg.sender);
 
-    emit FileUploaded(fileCount, _userId, _fileHash, _fileSize, _fileType, _fileName, _fileDescription, block.timestamp, msg.sender);
-    }
+    // user1.File[user1.fileCount] = File(
+    //   user1.fileCount,
+    //   _fileHash,
+    //   _fileSize,
+    //   _fileType,
+    //   _fileName,
+    //   _fileDescription,
+    //   block.timestamp,
+    //   msg.sender
+    // );
+
+    emit FileUploaded(
+      1,
+      _userId,
+      _fileHash,
+      _fileSize,
+      _fileType,
+      _fileName,
+      _fileDescription,
+      block.timestamp,
+      msg.sender
+    );
+  }
 }
-
