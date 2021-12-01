e6d1f9e0b1af83b2a15ebd3e903159a09002c5d5
Davide Lepri <davidelepri3@gmail.com>
2021-12-01 12:02:08 -0500

    File model and association created

--- server/db/index.js
+++ server/db/index.js
@@ -3,12 +3,16 @@
 const db = require('./db')
 
 const User = require('./models/User')
+const File = require('./models/File');
 
 //associations could go here!
+File.belongsTo(User)
+User.hasMany(File)
 
 module.exports = {
   db,
   models: {
     User,
+    File
   },
 }
