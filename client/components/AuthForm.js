b4d6a6f16f53cacc7824cd4d160cf9fb6a57fd42
David Kremenitzer <dkrem21@gmail.com>
2021-12-03 16:49:08 -0500

    finished login/signup page

--- client/components/AuthForm.js
+++ client/components/AuthForm.js
@@ -1,91 +1,111 @@
-import React from 'react'
-import {connect} from 'react-redux'
-import {authenticate} from '../store'
+import React from 'react';
+import { connect } from 'react-redux';
+import { authenticate } from '../store';
+import { Link } from 'react-router-dom';
 
-/**
- * COMPONENT
- */
-const AuthForm = props => {
-  const {name, displayName, handleSubmit, error} = props
+const AuthForm = (props) => {
+  const { name, displayName, handleSubmit, error } = props;
 
   return (
-    <div className='nav-margin'>
-      <form onSubmit={handleSubmit} name={name}>
-        <div>
-          <label htmlFor="username">
-            <small>Username</small>
-          </label>
-          <input name="username" type="text" />
-        </div>
-        <div>
-          <label htmlFor="password">
-            <small>Password</small>
-          </label>
-          <input name="password" type="password" />
-        </div>
-        {name === 'signup' ? (
-        <div>
-          <label htmlFor="email">
-            <small>Email</small>
-          </label>
-          <input name="email" type="email" />
-        </div>
-        ) : '' }
-        <div>
-          <button type="submit">{displayName}</button>
-        </div>
-        {error && error.response && <div> {error.response.data} </div>}
-      </form>
+    <div className='login'>
+      <div className='login__container'>
+        <img
+          className='login__logo'
+          src='https://www.kindpng.com/picc/m/235-2351000_login-icon-png-transparent-png.png'
+          alt=''
+        />
+        <form onSubmit={handleSubmit} name={name}>
+          <div>
+            <label htmlFor='username'></label>
+            <input
+              className='inputs'
+              name='username'
+              type='text'
+              placeholder='Username*'
+            />
+          </div>
+          <div>
+            <label htmlFor='password'></label>
+            <input
+              className='inputs'
+              name='password'
+              type='password'
+              placeholder='Password*'
+            />
+          </div>
+          {name === 'signup' ? (
+            <div>
+              <label htmlFor='email'></label>
+              <input
+                className='inputs'
+                name='email'
+                type='email'
+                placeholder='Email*'
+              />
+            </div>
+          ) : (
+            ''
+          )}
+          <div>
+            <div className='noAccount'>
+              {name !== 'signup' ? (
+                <h4>
+                  <Link className='linky' to='/signup'>
+                    {' '}
+                    Don't have an account? Sign Up
+                  </Link>
+                </h4>
+              ) : (
+                ''
+              )}
+            </div>
+            <button className='login__signInButton' type='submit'>
+              {displayName}
+            </button>
+          </div>
+          {error && error.response && <div> {error.response.data} </div>}
+        </form>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-/**
- * CONTAINER
- *   Note that we have two different sets of 'mapStateToProps' functions -
- *   one for Login, and one for Signup. However, they share the same 'mapDispatchToProps'
- *   function, and share the same Component. This is a good example of how we
- *   can stay DRY with interfaces that are very similar to each other!
- */
-const mapLogin = state => {
+const mapLogin = (state) => {
   return {
     name: 'login',
     displayName: 'Login',
-    error: state.auth.error
-  }
-}
+    error: state.auth.error,
+  };
+};
 
-const mapSignup = state => {
+const mapSignup = (state) => {
   return {
     name: 'signup',
     displayName: 'Sign Up',
-    error: state.auth.error
-  }
-}
+    error: state.auth.error,
+  };
+};
 
-const mapDispatch = dispatch => {
+const mapDispatch = (dispatch) => {
   return {
     handleSubmit(evt) {
-      evt.preventDefault()
+      evt.preventDefault();
 
       if (evt.target.name === 'login') {
-
-        const formName = evt.target.name
-        const username = evt.target.username.value
-        const password = evt.target.password.value
-        dispatch(authenticate(username, password, formName))
-
+        const formName = evt.target.name;
+        const username = evt.target.username.value;
+        const password = evt.target.password.value;
+        dispatch(authenticate(username, password, formName));
       } else {
-        
-        const formName = evt.target.name
-        const username = evt.target.username.value
-        const password = evt.target.password.value
-        const email = evt.target.email.value
-        dispatch(authenticate(username, password, formName, email))
+        const formName = evt.target.name;
+        const username = evt.target.username.value;
+        const password = evt.target.password.value;
+        const email = evt.target.email.value;
+        dispatch(authenticate(username, password, formName, email));
       }
-    }
-  }
-}
+    },
+  };
+};
 
-export const Login = connect(mapLogin, mapDispatch)(AuthForm)
-export const Signup = connect(mapSignup, mapDispatch)(AuthForm)
+export const Login = connect(mapLogin, mapDispatch)(AuthForm);
+export const Signup = connect(mapSignup, mapDispatch)(AuthForm);
