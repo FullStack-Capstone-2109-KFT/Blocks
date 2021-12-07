d503e0bc1e43582c1299cc2fbdbf2ef0c5da246b
David Kremenitzer <dkrem21@gmail.com>
2021-12-07 09:26:03 -0500

    updated more styling and text

--- client/components/Video.js
+++ client/components/Video.js
@@ -1,13 +1,12 @@
-import React from "react";
+import React from 'react';
 import { Player } from 'video-react';
-
 
 function Duda(props) {
   return (
     <Player>
-      <source src="https://api.renderforest.com/api/v1/projects/54831296/download?key=c2f33e89b2fec69ce060a0b677d0c010:410d9ad34331c541b4ba233a19c9aa2a63a5eb91c732d0d41a53f94d5b148678c514758edb661d64254ec8af2c6b92a3" />
+      <source src='https://api.renderforest.com/api/v1/projects/54831296/download?key=c2f33e89b2fec69ce060a0b677d0c010:410d9ad34331c541b4ba233a19c9aa2a63a5eb91c732d0d41a53f94d5b148678c514758edb661d64254ec8af2c6b92a3' />
     </Player>
   );
 }
 
-export default Duda
+export default Duda;
