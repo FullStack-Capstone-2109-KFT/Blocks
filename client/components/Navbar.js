5a830ba847fd6ad9ea669150bf39fbb424b60f4f
J-Oetting <joetting71@gmail.com>
2021-12-07 17:52:09 -0500

    hash link problem

--- client/components/Navbar.js
+++ client/components/Navbar.js
@@ -2,12 +2,7 @@ import React from "react";
 import { connect } from "react-redux";
 import { Link, useHistory } from "react-router-dom";
 import { logout } from "../store";
-<<<<<<< HEAD
-import { HashLink } from 'react-router-hash-link'
-=======
-import { HashLink as HashLink} from 'react-router-hash-link';
->>>>>>> main
-
+import HashLink from 'react-router-hash-link';
 export class Navbar extends React.Component {
 
   render() {
