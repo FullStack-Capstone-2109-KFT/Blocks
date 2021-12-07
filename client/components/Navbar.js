f7baed4b2fd1254f6d78e1c0af16014f1135ba39
Davide Lepri <davidelepri3@gmail.com>
2021-12-07 16:43:13 -0500

    changes

--- client/components/Navbar.js
+++ client/components/Navbar.js
@@ -2,11 +2,7 @@ import React from "react";
 import { connect } from "react-redux";
 import { Link, useHistory } from "react-router-dom";
 import { logout } from "../store";
-<<<<<<< HEAD
 import { HashLink } from 'react-router-hash-link'
-=======
-import { HashLink as HashLink} from 'react-router-hash-link';
->>>>>>> main
 
 export class Navbar extends React.Component {
 
