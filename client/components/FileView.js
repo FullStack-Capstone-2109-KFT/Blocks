6a11f139a6046834084710ada4666337bb769cc5
J-Oetting <joetting71@gmail.com>
2021-12-08 15:50:52 -0500

    copy to clipboard functional

--- client/components/FileView.js
+++ client/components/FileView.js
@@ -179,6 +179,7 @@ export default class FileView extends Component {
                   <Share
                     seen={this.state.seen}
                     fileSeen={file.fileNumber}
+                    fileCID={file.fileHash}
                     fileSelected={this.state.selected}
                     toggle={this.togglePopup}
                   />
--- client/components/SharePopUp.js
+++ client/components/SharePopUp.js
@@ -3,23 +3,29 @@ import React, {Component} from 'react';
 export default class Share extends Component {
     constructor(){
         super();
-
+        this.state={
+            copied: false
+        }
         this.handleClick = this.handleClick.bind(this);
     }
 
     handleClick(){
         const name = event.target.name;
         if (name === 'close'){
+            this.setState({
+                copied: false
+            })
             this.props.toggle();
         }
     }
     
     render(){
-        
+        console.log(this.state.copied);
         return (
             <div className='popup display-block'>
                 { this.props.seen && (this.props.fileSeen === this.props.fileSelected ) ? 
-                    <div className='popup-main'>
+                    this.state.copied === false ? 
+                    (<div className='popup-main'>
                         <div>
                             <button className='close_button' name='close' onClick={this.handleClick}>x</button>
                             <div>
@@ -28,10 +34,25 @@ export default class Share extends Component {
                             </div>
                             <div className='share_buttons'>
                                 <button>QR Code</button>
-                                <button>Copy to Clipboard</button>
+                                <button onClick={() => {
+                                    navigator.clipboard.writeText(this.props.fileCID);
+                                    this.setState({copied: true})}}>Copy to Clipboard</button>
                             </div>
                         </div> 
-                    </div> : null
+                    </div>) : 
+                    (<div className='popup-main'>
+                        <div>
+                            <button className='close_button' name='close' onClick={this.handleClick}>x</button>
+                            <div>
+                                <h3 className='popup_head'>Sharing CID:</h3>
+                                <p className='popup_head'>file description</p>
+                            </div>
+                            <div className='share_buttons'>
+                                <h3>Your CID has been copied to your clipobaord.</h3>
+                                <h4>Cool beans!!</h4>
+                            </div>
+                        </div> 
+                    </div>) :  null
                 }
             </div>
         )
