bd42445ac089ac723f34150f776b28e6c850d045
Jordan Oetting <JOetting71@gmail.com>
2022-01-28 19:12:23 -0500

    css update for learn page

--- client/components/LandingPageComponents/Learn-Blocks.js
+++ client/components/LandingPageComponents/Learn-Blocks.js
@@ -3,7 +3,9 @@ import React from "react";
 
 const Learn = (props) => {
   return (
-    <h3 className="learnText">placeholder text for Learn about Blocks</h3>
+    <div id="about">
+      <h3 className="learnText">placeholder text for Learn about Blocks</h3>
+    </div>
   )
 }
 
