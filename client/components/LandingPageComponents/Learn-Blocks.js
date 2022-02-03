5b27e0bcffff1e121c7a794c20524ce8ddcdf696
Jordan Oetting <JOetting71@gmail.com>
2022-02-03 15:21:08 -0500

    centering text

--- client/components/LandingPageComponents/Learn-Blocks.js
+++ client/components/LandingPageComponents/Learn-Blocks.js
@@ -3,8 +3,13 @@ import React from "react";
 
 const Learn = (props) => {
   return (
-    <div id="about" className="learnText">
-      <h2>How Blocks Works</h2>
+    <div id="about" className="learnText" /* style="text-align: center" */>
+      <div>
+        <h2>How Blocks Works</h2>
+      </div>
+      <div>
+        <h5>It all starts with a file...</h5>
+      </div>
     </div>
   )
 }
