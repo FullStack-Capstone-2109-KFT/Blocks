3418cf46f0d1319e565e364544a73e7a94a69712
Jordan Oetting <JOetting71@gmail.com>
2022-02-03 13:01:17 -0500

    added title to about blocks section

--- client/components/LandingPageComponents/Learn-Blocks.js
+++ client/components/LandingPageComponents/Learn-Blocks.js
@@ -3,8 +3,8 @@ import React from "react";
 
 const Learn = (props) => {
   return (
-    <div id="about">
-      <h3 className="learnText">placeholder text for Learn about Blocks</h3>
+    <div id="about" className="learnText">
+      <h2>How Blocks Works</h2>
     </div>
   )
 }
