11d52bb369b92ae8d130b16433e20e8db7ea44a9
Jordan Oetting <JOetting71@gmail.com>
2022-02-04 18:23:12 -0500

    added img attribution and font styling

--- client/components/LandingPageComponents/Learn-Blocks.js
+++ client/components/LandingPageComponents/Learn-Blocks.js
@@ -3,12 +3,15 @@ import React from "react";
 
 const Learn = (props) => {
   return (
-    <div id="about" className="learnText" /* style="text-align: center" */>
+    <div className="learnText" /* style="text-align: center" */>
       <div>
         <h2>How Blocks Works</h2>
       </div>
       <div>
         <h5>It all starts with a file...</h5>
+      </div>
+      <div>
+        <a href="https://www.flaticon.com/free-icons/file" title="file icons">File icons created by Roundicons Premium - Flaticon</a>
       </div>
     </div>
   )
